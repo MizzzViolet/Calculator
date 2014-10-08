4c8d0fe528599725395a6fc412a1acfabcd5e850
Jasmine <jasmine.tlkdr@gmail.com>
2014-10-08 21:48:13 +0100

    Calculator w/ Binary Converter

--- Calculator/JEViewController.h
+++ Calculator/JEViewController.h
@@ -8,6 +8,36 @@
 
 #import <UIKit/UIKit.h>
 
-@interface JEViewController : UIViewController
+@interface JEViewController : UIViewController{
+    int operation;
+    int number;
+    double total;
+    BOOL enterNumber;
+    IBOutlet UILabel * display;
+    
+}
 
+//@property (nonatomic) BOOL enterNumber;
+
+
+-(IBAction)Number1:(id)sender;
+-(IBAction)Number2:(id)sender;
+-(IBAction)Number3:(id)sender;
+-(IBAction)Number4:(id)sender;
+-(IBAction)Number5:(id)sender;
+-(IBAction)Number6:(id)sender;
+-(IBAction)Number7:(id)sender;
+-(IBAction)Number8:(id)sender;
+-(IBAction)Number9:(id)sender;
+-(IBAction)Number0:(id)sender;
+-(IBAction)Multiply:(id)sender;
+-(IBAction)Divide:(id)sender;
+-(IBAction)Subtract:(id)sender;
+-(IBAction)Plus:(id)sender;
+-(IBAction)Equals:(id)sender;
+-(IBAction)Clear:(id)sender;
+-(IBAction)Binary:(id)sender;
+-(IBAction)Moduluse:(id)sender;
+//-(IBAction)Point:(id)sender;
+-(IBAction)Delete:(id)sender;
 @end
--- Calculator/JEViewController.m
+++ Calculator/JEViewController.m
@@ -14,6 +14,338 @@
 
 @implementation JEViewController
 
+//@synthesize enterNumber = _enterNumber;
+//-(IBAction)NumPressed:(id)sender{
+//    number = number * 10 + (double)[sender tag];
+//   // number=[sender currentTitle];//get the current title of the button pressed
+//    display.text=[NSString stringWithFormat:@"%i", number];
+//    
+//  //  display.text = [NSString stringWithFormat:@"%.2f", number];
+////    NSString *digit = [sender currentTitle];
+////    display.text = [display.text stringByAppendingString:digit];
+//}
+-(IBAction)Number1:(id)sender{
+    number = number * 10;
+    number = number + 1;
+
+    display.text = [NSString stringWithFormat:@"%i", number];
+}
+-(IBAction)Number2:(id)sender{
+  
+    number = number * 10;
+    number = number + 2;
+   
+    display.text = [NSString stringWithFormat:@"%i", number];
+}
+-(IBAction)Number3:(id)sender{
+    number = number * 10;
+    number = number + 3;
+    
+    display.text = [NSString stringWithFormat:@"%i", number];
+}
+-(IBAction)Number4:(id)sender{
+    number = number * 10;
+    number = number + 4;
+    
+    display.text = [NSString stringWithFormat:@"%i", number];
+}
+-(IBAction)Number5:(id)sender{
+    number = number * 10;
+    number = number + 5;
+    
+    display.text = [NSString stringWithFormat:@"%i", number];
+}
+-(IBAction)Number6:(id)sender{
+    number = number * 10;
+    number = number + 6;
+    display.text = [NSString stringWithFormat:@"%i", number];
+}
+-(IBAction)Number7:(id)sender{
+    number = number * 10;
+    number = number + 7;
+    display.text = [NSString stringWithFormat:@"%i", number];
+}
+-(IBAction)Number8:(id)sender{
+    number = number * 10;
+    number = number + 8;
+    display.text = [NSString stringWithFormat:@"%i", number];
+}
+-(IBAction)Number9:(id)sender{
+    number = number * 10;
+    number = number + 9;
+    display.text = [NSString stringWithFormat:@"%i", number];
+}
+-(IBAction)Number0:(id)sender{
+    number = number * 10;
+    number = number + 0;
+    display.text = [NSString stringWithFormat:@"%i", number];
+}
+-(IBAction)Multiply:(id)sender{
+    
+    if(total == 0){
+        total = number;
+    }
+    else{
+        switch(operation){
+            case 1:
+                total = total * number;
+                break;
+                
+            case 2:
+                total = total / number;
+                break;
+            case 3:
+                total = total - number;
+                break;
+                
+            case 4:
+                total = total + number;
+                break;
+            case 5:
+                total = (int)total % (int)number;
+                break;
+            default:
+                break;
+        }
+    }
+   // enterNumber=NO;
+    operation = 1;
+    number = 0;
+}
+-(IBAction)Divide:(id)sender{
+  
+    if(total == 0){
+        total = number;
+    }
+    else{
+        switch(operation){
+            case 1:
+                total = total * number;
+                break;
+                
+            case 2:
+                total = total / number;
+                break;
+            case 3:
+                total = total - number;
+                break;
+                
+            case 4:
+                total = total + number;
+                break;
+            case 5:
+                total = (int)total % (int)number;
+                break;
+            default:
+                break;
+        }
+    }
+  //  enterNumber=NO;
+    operation = 2;
+    number = 0;
+}
+-(IBAction)Subtract:(id)sender{
+    
+    if(total == 0){
+        total = number;
+    }
+    else{
+        switch(operation){
+            case 1:
+                total = total * number;
+                break;
+                
+            case 2:
+                total = total / number;
+                break;
+            case 3:
+                total = total - number;
+                break;
+                
+            case 4:
+                total = total + number;
+                break;
+            case 5:
+                total = (int)total % (int)number;
+                break;
+            default:
+                break;
+        }
+    }
+   // enterNumber=NO;
+    operation = 3;
+    number = 0;
+}
+-(IBAction)Plus:(id)sender{
+    
+    if(total == 0){
+        total = number;
+    }
+    else{
+        switch(operation){
+            case 1:
+                total = total * number;
+                break;
+                
+            case 2:
+                total = total / number;
+                break;
+            case 3:
+                total = total - number;
+                break;
+                
+            case 4:
+                total = total + number;
+                break;
+            case 5:
+                total = (int)total % (int)number;
+                break;
+            default:
+                break;
+        }
+    }
+  //  enterNumber=NO;
+    operation = 4;
+    number = 0;
+    
+}
+-(IBAction)Equals:(id)sender{
+    //number = [display text];
+    if(total == 0){
+        total = number;
+    }
+    else{
+        switch(operation){
+            case 1:
+                total = total * number;
+                break;
+                
+            case 2:
+                total = total / number;
+                break;
+            case 3:
+                total = total - number;
+                break;
+                
+            case 4:
+                total = total + number;
+                break;
+            case 5:
+                total = (int)total % (int)number;
+                break;
+            default:
+                break;
+        }
+    }
+    
+    operation = 0;
+    number = 0;
+    display.text = [NSString stringWithFormat:@"%.2f",total];
+}
+-(IBAction)Clear:(id)sender{
+    operation = 0;
+    total = 0;
+    number = 0;
+  //  enterNumber=NO;
+    display.text = [NSString stringWithFormat:@"0"];
+}
+-(IBAction)Binary:(id)sender{
+    NSMutableString * string = [[NSMutableString alloc] init];
+    
+   // int spacing = pow( 2, 3 );
+    //int width = ( sizeof( number ) ) * spacing;
+    int width = ( sizeof( number ) );
+    int binaryDigit = 0;
+    int integer = number;
+    
+    while( binaryDigit < width )
+    {
+        binaryDigit++;
+        
+        [string insertString:( (integer & 1) ? @"1" : @"0" )atIndex:0];
+        
+       // if( binaryDigit % spacing == 0 && binaryDigit != width )
+        if(binaryDigit != width )
+        {
+            [string insertString:@" " atIndex:0];
+        }
+        
+        integer = integer >> 1;
+    }
+    display.text = [NSString stringWithFormat:@"%@",string];
+   
+//          NSString *binary = @"";
+//          int two = 2;
+//          int copy1 = number;
+//          int copy2 = 0;
+//          int count = 0;
+//          while (copy1 != 0) {
+//              copy2 = copy1;
+//              copy1 /= two;
+//              count++;
+//          }
+//          int bin [count];
+//          for (int i = 0; i < count; i++) {
+//              copy2 = number;
+//              number /= two; // copy1 = 7/2 -> copy1 = 3
+//              copy2 = copy2 % two; // copy2 = 1
+//              bin[i] = copy2;
+//          }
+//          for (int i = count - 1; i >= 0; i--) {
+//              *binary += [@(bin[i]) stringValue];
+//          }
+//           display.text = [NSString stringWithFormat:@"%@",binary];
+    //      int bib = 12;
+  //  binary = Integer.toBinaryString(num);
+    //System.out.print(Integer.toBinaryString(bib));
+    
+}
+
+-(IBAction)Delete:(id)sender{
+    number = number/10;
+    display.text = [NSString stringWithFormat:@"%i",number];
+}
+
+//-(IBAction)Point:(id)sender{
+//
+//    
+//    NSRange range = [display.text rangeOfString:@"."];
+//    if (range.location ==NSNotFound && !enterNumber){
+//       display.text = [ display.text stringByAppendingFormat:@"."];
+//    }
+//}
+-(IBAction)Moduluse:(id)sender{
+    
+    if(total == 0){
+        total = number;
+    }
+    else{
+        switch(operation){
+            case 1:
+                total = total * number;
+                break;
+                
+            case 2:
+                total = total / number;
+                break;
+            case 3:
+                total = total - number;
+                break;
+                
+            case 4:
+                total = total + number;
+                break;
+            case 5:
+                total = (int)total % (int)number;
+                break;
+            default:
+                break;
+        }
+    }
+    enterNumber=NO;
+    operation = 5;
+    number = 0;
+}
 - (void)viewDidLoad
 {
     [super viewDidLoad];
